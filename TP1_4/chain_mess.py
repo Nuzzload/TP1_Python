9256d7be26b9f44561b3b4f479ffd15fc9993b09
nuzzload <isdhimene@outlook>
2025-09-26 00:41:06 +0200

    TP1.4 fonctionnel

--- TP1_4/chain_mess.py
+++ TP1_4/chain_mess.py
@@ -1,5 +1,18 @@
+from tools import checkIntPos
+
 
 def chain_mess():
     print("TP1_4")
+    message = input("Entrez le message à souhaiter : ")
 
-    
+    nombre = checkIntPos()
+
+    amis = []
+    for i in range (1, nombre + 1):
+        nom = input(f"Nom de l'ami {i}").strip()
+        if nom:
+            amis.append(nom)
+
+    print("Messages personalisés :")
+    for ami in amis:
+        print(f"Cher {ami}, {message}")
